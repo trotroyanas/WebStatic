ab7ad3b58a1b8169eeee374255e6b7383f84b016
Trotroyanas <trotroyanas@gmail.com>
2019-01-11 12:31:18 +0100

    2e commit

--- src/javascripts/scripts.js
+++ src/javascripts/scripts.js
@@ -1,1 +1,5 @@
 // Add your scripts here
+import tst from './test.js';
+
+let val = tst.hello();
+let tt = tst.zozo();
---
+++ src/javascripts/test.js
@@ -0,0 +1,11 @@
+var $ = require("jquery");
+
+
+module.exports = {
+  hello: function () {
+    $("#tst").html("Mon nouveau test...");
+  },
+  zozo: function () {
+    $("#tst").html("Mon 2e test...");
+  }
+}
